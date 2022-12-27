621428d9a6a6448b61f19cbaacf9d7cbae3f0989
masudxr <masudxr@example.com>
2022-12-27 20:42:29 +0600

    need to watch previous useState

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,28 +1,107 @@
 /* eslint-disable no-undef */
-import { useState } from 'react';
 
+// function Todo() {
+//     const [todo, setTodo] = useState('');
+//     const [warning, setWarning] = useState(null);
+
+//     const handleInput = (e) => {
+//         const inputValue = e.target.value;
+//         const updatedwarning = inputValue.includes('.js') ? 'You need JS Knowlage' : null;
+
+//         setTodo(inputValue);
+
+//         setWarning(updatedwarning);
+//     };
+//     return (
+//         <div>
+//             <p> {todo} </p>
+//             <p>
+//                 <textarea name="todo" value={todo} onChange={handleInput} />
+//             </p>
+//             <hr />
+//             <h2>{warning || 'Good Choice'}</h2>
+//         </div>
+//     );
+// }
+// -----------------------------------------------
+// useState with previous state
 function Todo() {
-    const [todo, setTodo] = useState('');
-    const [warning, setWarning] = useState(null);
-
-    const handleInput = (e) => {
-        const inputValue = e.target.value;
-        const updatedwarning = inputValue.includes('.js') ? 'You need JS Knowlage' : null;
-
-        setTodo(inputValue);
-
-        setWarning(updatedwarning);
-    };
+    const [todo, setTodo] = useState({
+        title: '',
+        description: '',
+    });
+    const { title, description } = todo;
     return (
         <div>
-            <p> {todo} </p>
+            <p> {title} </p>
             <p>
-                <textarea name="todo" value={todo} onChange={handleInput} />
+                <input
+                    type="text"
+                    value={title}
+                    onChange={(e) =>
+                        setTodo({
+                            ...todo,
+                            title: e.target.value,
+                        })
+                    }
+                />
             </p>
-            <hr />
-            <h2>{warning || 'Good Choice'}</h2>
+            <p> {description} </p>
+            <p>
+                <textarea
+                    name="todo"
+                    value={description}
+                    onChange={(e) =>
+                        setTodo({
+                            ...todo,
+                            description: e.target.value,
+                        })
+                    }
+                />
+            </p>
         </div>
     );
 }
+// -----------------------------------
+
+// useState with object/Array
+
+// function Todo() {
+//     const [todo, setTodo] = useState({
+//         title: '',
+//         description: '',
+//     });
+//     const { title, description } = todo;
+//     return (
+//         <div>
+//             <p> {title} </p>
+//             <p>
+//                 <input
+//                     type="text"
+//                     value={title}
+//                     onChange={(e) =>
+//                         setTodo({
+//                             ...todo,
+//                             title: e.target.value,
+//                         })
+//                     }
+//                 />
+//             </p>
+//             <p> {description} </p>
+//             <p>
+//                 <textarea
+//                     name="todo"
+//                     value={description}
+//                     onChange={(e) =>
+//                         setTodo({
+//                             ...todo,
+//                             description: e.target.value,
+//                         })
+//                     }
+//                 />
+//             </p>
+//         </div>
+//     );
+// }
 
 export default Todo;
